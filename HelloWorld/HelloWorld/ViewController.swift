d8380f0714e5176f3a1d195805ae00e1c7f2598c
deepakedward <deepakedward18@gmail.com>
2017-08-13 21:45:32 -0700

    First iOS App - Hello World!

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Deepak Edward on 8/12/17.
+//  Copyright © 2017 Deepak Edward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func welcomePressed(_ sender: Any) {
+        background.isHidden = false
+        titleImage.isHidden = false
+        welcomeBtn.isHidden = true
+    }
+
+}
+
